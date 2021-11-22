4797dc8515a4cd9f760e3a6903f316913c4f4c50
Corey <coreyshanahan02@gmail.com>
2021-11-22 18:29:53 +0000

    Added Persistence

---
+++ src/main/java/controllers/mainViewController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import main.Driver;
+
+public class mainViewController {
+
+    public void save(){
+        try{
+            Driver.save();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void load(){
+        try {
+            Driver.load();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void clear(){
+        Driver.auctionAPI.clear();
+    }
+}
